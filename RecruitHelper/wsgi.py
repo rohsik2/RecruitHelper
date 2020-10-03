42d06fe4e225fa56737ffb06bd6043a1fff2f7f4
rohsik2 <rohsik2@gmail.com>
2020-10-03 08:11:14 +0000

    wow

--- RecruitHelper/wsgi.py
+++ RecruitHelper/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+path = '/home/RecruitHelper/RecruitHelper'  # PythonAnywhere 계정으로 바꾸세요.
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'RecruitHelper.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RecruitHelper.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
